9a92131ecfdaebc5d318b8947146cb36e27962f7
raajteja26 <98390867+raajteja26@users.noreply.github.com>
2023-06-22 10:44:36 +0530

    last

--- frontend/frontend-app/src/components/navbar.js
+++ frontend/frontend-app/src/components/navbar.js
@@ -15,8 +15,8 @@ function navbar() {
         <Navbar bg="light" style={{paddingTop:"0px",paddingBottom:"0px"}}>
         <Container fluid style={{marginTop:"-3px"}}>
         <Row>
-        <Col style={{marginTop:"5px"}}><Navbar.Brand style={{fontWeight:"600px"}} href="/"><img src={favicon} style={{width:"25%",height:"60%"}} alt="R"/><span style={{fontWeight:"500"}}>aajteja.</span></Navbar.Brand></Col>
-        <Col style={{marginTop:"5px"}}><Navbar.Brand style={{fontWeight:"600px"}} href="/projects">Projects</Navbar.Brand></Col>
+        <Col ><Navbar.Brand style={{fontWeight:"600px"}} href="/"><img src={favicon} style={{width:"25%",height:"60%"}} alt="R"/><span style={{fontWeight:"500"}}>aajteja.</span></Navbar.Brand></Col>
+        <Col ><Navbar.Brand href="/projects">Projects</Navbar.Brand></Col>
         {localStorage.getItem("token") ? <Col style={{marginTop:"5px"}}><Navbar.Brand style={{fontWeight:"600px"}} href="/admin">Admin</Navbar.Brand></Col> : ""}
         </Row>
         <Navbar.Brand style={{fontWeight:"600px"}}>{localStorage.getItem('token') ? <h6 style={{cursor:"pointer"}} onClick={handleLogout}>Logout</h6> : <LoginPage placement='end' name='Login'/>}</Navbar.Brand>
